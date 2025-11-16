dfa2bfab7ab3b10b273144da944ec625460d5cd4
Manohar Singh <burathimannu@gmail.com>
2025-11-16 12:52:56 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="Mannu1">
+      <h1>Manohar Singh</h1>
+      <h2>B.Tech Graduate from GEHU, Dehradun</h2>
+    </div>
+    <div id="Mannu2">
+      <h1>Hail from Champawat, Uttarakhand</h1>
+      <h3>Working as a PAT at Cognizant Technology Solutions, Pune</h3>
+    </div>
+    </>
+  )
+}
+
+export default App
